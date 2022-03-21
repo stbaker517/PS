1c7a75fa6e209f1e32b754f80f52f4b05c12e463
이원규 이원규 <stbaker517@gmail.com>
2022-03-21 13:29:50 +0900

    220321

---
+++ BACKJOON/20500/bj20500.cpp
@@ -0,0 +1,14 @@
+#include<bits/std++.h>
+using namespace std;
+
+int dp[1516][2];
+int left[1516][2];//[n][0]=1 [n][1]=5
+int main(){
+
+    left[0][0]=1;
+    left[0][1]=5;
+
+    
+
+    return 0;
+}
